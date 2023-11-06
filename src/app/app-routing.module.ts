7f79086bd3b1725055fdf6dfa76b0465abd3b592
preetam-sharma <preetamsharma915@gmail.com>
2023-11-06 16:25:25 +0530

    data updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ProjectComponent } from './pages/project/project.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent,
+  },
+  {
+    path: 'project',
+    component: ProjectComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 
+import { LoadScriptDirective } from './directives/app-load-script.directive';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ProjectComponent } from './pages/project/project.component';
+
 @NgModule({
   declarations: [
+    LoadScriptDirective,
     AppComponent,
     HomeComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    PortfolioComponent,
+    ProjectComponent,
   ],
   imports: [
     BrowserModule,
